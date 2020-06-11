bf5307b06e31a62f2722c1581ce1c45d757e9ca8
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-11 20:38:56 +0200

    created a custom reducer hook for localStorage

---
+++ src/Hooks/useLocalStorageReducer.js
@@ -0,0 +1,20 @@
+import { useReducer, useEffect } from 'react';
+
+export default function useLocalStorageReducer(key, defaultVal, reducer) {
+  // make piece of state , based off of value in localStorage or default
+  const [state, dispatch] = useReducer(reducer, defaultVal, () => {
+    let value;
+    try {
+      value = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      value = defaultVal;
+    }
+    return value;
+  });
+  // use useEffect to update localStorage when state change
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, dispatch];
+}
