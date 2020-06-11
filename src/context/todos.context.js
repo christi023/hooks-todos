556fad385a9473f2670e6ca9e255e7eddbb89f4d
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-11 19:35:22 +0200

    split contexts

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,18 +1,24 @@
 // todos
 // all methods to interact with todos
-import React, { createContext } from 'react';
-// Hooks
-import useTodoState from '../Hooks/useTodoState';
+import React, { createContext, useReducer } from 'react';
+// reducers
+import todoReducer from '../reducers/todo.reducer';
 
 const defaultTodos = [
   { id: 1, task: 'Learn Web Development', completed: false },
   { id: 2, task: 'Relax with the family', completed: true },
 ];
-// create context
+// create context and splitting contexts
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
+
 // make todos provider
 export function TodosProvider(props) {
-  const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
 
-  return <TodosContext.Provider value={todosStuff}>{props.children}</TodosContext.Provider>;
+  return (
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}> {props.children}</DispatchContext.Provider>
+    </TodosContext.Provider>
+  );
 }
